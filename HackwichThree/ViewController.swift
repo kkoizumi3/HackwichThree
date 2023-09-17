34b449fd68b45f8a29cc56b6a5ab74831d287c55
Kaci Koizumi <koizumi3@hawaii.edu>
2023-09-16 23:14:09 -1000

    Finished on your own section

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController
 {
-var firstString = "The background color will turn blue"
+    var firstString = "The background color will turn blue"
     
-var secondString = "The background color will turn green"
-
-var thirdString = "cool"
+    var secondString = "The background color will turn green"
     
+    var thirdString = "cool"
+    
+    let num = 3
+    
+    let numnum = 0
     
     @IBOutlet weak var firstLabel: UILabel!
     
@@ -23,21 +26,52 @@ var thirdString = "cool"
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func changeColorButtonPressed(_ sender: Any)
     {
         if firstString == "The background color will turn red"
         {
             self.view.backgroundColor = UIColor.red
         }
-       else
+        else
         {
-           self.view.backgroundColor = UIColor.blue
-           
-           self.firstLabel.text = "This is \(thirdString)"
-       }
+            self.view.backgroundColor = UIColor.blue
+            
+            self.firstLabel.text = "This is \(thirdString)"
+        }
         
     }
     
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBAction func pressTextChange(_ sender: Any)
+    {
+        if thirdString == "cool"
+        {
+            self.secondLabel.text = "yippee"
+        }
+        else
+        {
+            self.thirdLabel.text = "yuh"
+        }
+        
+        
+    }
+    @IBAction func additionButton(_ sender: Any)
+    {
+        if num == numnum
+        {
+            print("The code in this block will not execute")
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.green
+            let add = num + numnum
+            print(add)
+        }
+        
+    }
+    
+    
 }
-
